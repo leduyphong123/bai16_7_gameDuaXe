3bedc4a0b6dd079c7ca2ec067b940a27358ff4d9
leduyphong123 <leduyphongck789@gmail.com>
2023-04-20 11:03:35 +0700

    fix animation

--- index.js
+++ index.js
@@ -1,54 +1,32 @@
-// let ctx=document.getElementById("canvasMoto").getContext("2d");
-// let imgObj= new Image();
-// let backObj= new Image();
-// backObj.onload=function(){
-//     ctx.drawImage(backObj,0,0,1400,700);
-// }
+let ctx = document.getElementById("canvasMoto");
+let canvasMoto = ctx.getContext("2d");
 
-// let moto=new Moto(5,5);
+ctx.width = 1100;
+ctx.height = 560;
 
-// imgObj.onload=function(){
-//     ctx.drawImage(imgObj,700,650,40,60);
-// }
-
-// function Moto(){
-//     this.x=5;
-//     this.y=5;
-//     this.setX=function(value){
-//         this.x=this.x+value;
-//         return this.x;
-//     }
-//     this.setY= function(value){
-//         this.y=this.y+value;
-//         return this.y;
-//     }
-// }
-function startGame() {
-    myGameMoto.start();
-}
-let myGameMoto = {
-    canvas: document.getElementById("canvasMoto"),
-    start: function () {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+function Moto(){
+    this.x=0;
+    this.y=0;
+ 
+    this.update = function(){
+        this.x+=1;
+        canvasMoto.clearRect(0,0,ctx.width,ctx.height);
+        canvasMoto.fillStyle="#f01c11";
+        canvasMoto.fillRect(this.x,0,40,60);
     }
 }
 
-function Moto(x, y) {
-    this.width = 40;
-    this.height = 60;
-    this.x = x;
-    this.y = y;
-    ctx = myGameMoto.context;
-    let imgObj = new Image();
+let moto=new Moto();
 
-    imgObj.onload = function () {
-        ctx.drawImage(imgObj, 0, 0, 40, 60);
-    }
-    imgObj.src="https://media.istockphoto.com/id/1151058903/vi/vec-to/xe-%C4%91ua-nh%C3%ACn-t%E1%BB%AB-tr%C3%AAn-cao.jpg?s=1024x1024&w=is&k=20&c=ujfE33-DOR4cnS5yQegfdYvn1gr0HOtaw-JhySVtBSo=";
+function Hello(){
+
+    moto.update();
 }
+
+let game=setInterval(Hello,1000/60);
+
+// moto.updata();
+// window.onload=auto();
+// imgObj.src="https://media.istockphoto.com/id/1151058903/vi/vec-to/xe-%C4%91ua-nh%C3%ACn-t%E1%BB%AB-tr%C3%AAn-cao.jpg?s=1024x1024&w=is&k=20&c=ujfE33-DOR4cnS5yQegfdYvn1gr0HOtaw-JhySVtBSo=";
 
 // backObj.src="https://st.gamevui.com/images/image/2017/08/21/duong-dua-khac-nghiet-hd1.jpg";
